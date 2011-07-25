98cc069d617864ed7dbde59ff697d6b34736f245
SeungBum Kim <picxenk@gmail.com>
2011-07-25 20:52:00 +0900

    설정 JSON 오류 수정

--- sio_server.js
+++ sio_server.js
@@ -2,7 +2,7 @@
 require.paths.unshift(__dirname + '/server')
 var http = require('http');
 var fs = require('fs');
-var ws = require('websocket-server');
+var io = require('socket.io');
 var mongo = require('mongodb');
 var static = require('node-static');
 var EpicX = require('epicx').EpicX;
@@ -14,9 +14,13 @@ var config = {};
 var encoding = 'utf-8';
 var config_file_name = 'config.json';
 try {
-    config = JSON.parse(fs.readFileSync(config_file_name, encoding));
+    config = JSON.parse(fs.readFileSync(config_file_name, encoding).toString());
 } catch(err) {
-    console.error(config_file_name + ' - file does not exist, please make the config file');
+    if (err.errno == 9) {
+        console.error(config_file_name + ' - file does not exist, please make the config file');
+    } else {
+        console.error(err);
+    }
     return;
 }
 
@@ -25,35 +29,33 @@ var db = new mongo.Db(config.db, new mongo.Server(config.mongo_host, config.mong
 db.addListener('error', function(error) {
     console.log('[Error]: connection to MongoDB');
 });
-var server = ws.createServer({debug: config.debug});
+
 var file = new(static.Server)(config.public_path);
 var http_server = http.createServer(function(req, res) {
     req.addListener('end', function() {
         file.serve(req, res);
     });
 });
-
-/** START HTTP FILE SERVER **/
+io = io.listen(http_server);
 http_server.listen(config.http_port, config.host_ip);
 console.log('[Message]: HTTP file server running at http://' + config.host_ip + ':' + config.http_port);
+
 
 /** START EpicX WEBSOCKET SERVER + DB **/
 db.open(function(c_db) {
     var epicx = new EpicX(db);
 
-    server.on('connection', function(con) {
+    io.sockets.on('connection', function(con) {
         epicx.connect(con);
         con.on('close', function() {epicx.close(con)});
         con.on('message', function(msg) {epicx.run(con, msg)});
     });
 
-    server.on('error', function(){
+    io.sockets.on('error', function(){
         console.log(Array.prototype.join.call(arguments, ", "));
     });
 
-    server.on('disconnected', function(con) {
+    io.sockets.on('disconnected', function(con) {
         console.log(con.id+"discon");
     });
-    server.listen(config.ws_port);
-    console.log('[Message]: WebSocket server running at http://' + config.host_ip + ':' + config.ws_port);
 });
